f56534ba5d5a6449eee47c3621f97ed0b356c527
Wester Guimarães Martins <wgmartins@hotmail.com>
2020-03-29 15:36:48 -0300

    Aulas primeiro programa, configuração de central de exercícios e comentários.

---
+++ src/CursoCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CursoCSharp.Fundamentos;
+using System;
+using System.Collections.Generic;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>()
+            {
+                {"Primeiro Prgrama - Fundamentos", PrimeiroPrograma.Executar },
+                {"Comentários - Fundamentos", Comentarios.Executar }
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
