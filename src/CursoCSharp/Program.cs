d572a38324471f8bbd27200663eed2f9701d7a41
Wester Guimarães Martins <wgmartins@hotmail.com>
2020-04-19 16:49:46 -0300

    Aulas: 17 - Variáveis e Constantes 18 - Inferência de Tipo 19 - Interpolação de String 20 - Notação Ponto 21 - Lendo Dados do Console 22 - Formatando Números 23 - Conversões

--- src/CursoCSharp/Program.cs
+++ src/CursoCSharp/Program.cs
@@ -11,7 +11,14 @@ namespace CursoCSharp
             var central = new CentralDeExercicios(new Dictionary<string, Action>()
             {
                 {"Primeiro Prgrama - Fundamentos", PrimeiroPrograma.Executar },
-                {"Comentários - Fundamentos", Comentarios.Executar }
+                {"Comentários - Fundamentos", Comentarios.Executar },
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
+                {"Inferência - Fundamentos", Inferencia.Executar },
+                {"Interpolação - Fundamentos", Interpolacao.Executar },
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar },
+                {"Lendo Dados - Fundamentos", LendoDados.Executar },
+                {"Formatando Número - Fundamentos", FormatandoNumero.Executar },
+                {"Conversões - Fundamentos", Conversoes.Executar },
             });
 
             central.SelecionarEExecutar();
