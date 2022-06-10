bcacf81ca1f406eae0dc2861552a79278f46db59
Wester Guimarães Martins <wgmartins@hotmail.com>
2022-06-09 21:45:25 -0300

    Aulas: 55 - Parâmetros Variáveis 56 - Parâmetros Nomeados 57 - Getters & Setters

--- src/CursoCSharp/Program.cs
+++ src/CursoCSharp/Program.cs
@@ -48,6 +48,9 @@ namespace CursoCSharp
                 {"Métodos Estáticos  - Classes e Métodos", MetodosEstaticos.Executar },
                 {"Atributos Estáticos  - Classes e Métodos", AtributosEstaticos.Executar },
                 {"Desafio Atributo  - Classes e Métodos", DesafioAtributo.Executar },
+                {"Params  - Classes e Métodos", Params.Executar },
+                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar },
+                {"Get Set - Classes e Métodos", GetSet.Executar },
             });
 
             central.SelecionarEExecutar();
